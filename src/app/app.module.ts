b7a7bd65339b74956387b054d5be84a1a6c755ab
Biswaranjan Maharana <bmaharana866@gmail.com>
2020-07-30 13:10:16 +0530

    updated material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
-import 'hammerjs';
 import { from } from 'rxjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';

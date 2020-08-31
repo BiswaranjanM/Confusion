3f4c9d2ed306b5fedd96bfae171787f6d4843def
Biswaranjan Maharana <bmaharana866@gmail.com>
2020-08-31 18:37:32 +0530

    Promise part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import {MatListModule} from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 import { MenuComponent } from './menu/menu.component';
@@ -63,6 +65,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatSelectModule,
     MatSlideToggleModule,
     ReactiveFormsModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     DishService,
